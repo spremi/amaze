3973524fd093f6a4be713ebdc814933da5b8656b
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:29:51 +0530

    chore: c: app: Autofix lint errors
    Command:
      ng lint --fix=true
    
    Manually fix 2 errors that couldn't be fixed.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('amaze-client app is running!');
+    expect(compiled.querySelector('.content span').textContent)
+      .toContain('amaze-client app is running!');
   });
 });
