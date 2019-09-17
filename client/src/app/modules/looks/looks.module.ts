340f04d1e88cc3e157c687d2990598d3f0ee60e4
Sanjeev Premi <spremi@ymail.com>
2019-09-18 01:51:40 +0530

    chore: c: modules: looks: Re-export commonly used material modules
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/modules/looks/looks.module.ts
+++ client/src/app/modules/looks/looks.module.ts
@@ -9,11 +9,49 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ],
+  providers: [
+    MatIconRegistry,
+  ],
+  exports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
 })
 export class LooksModule { }
