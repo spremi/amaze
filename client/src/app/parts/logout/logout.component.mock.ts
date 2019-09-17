edae9ea6aea18808148d0319aaf48e7373da8f10
Sanjeev Premi <spremi@ymail.com>
2019-09-18 00:20:48 +0530

    test: c: parts: logout: Add mock
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/logout/logout.component.mock.ts
@@ -0,0 +1,15 @@
+//
+// [amaze-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-logout',
+  template: '<span>Mock Logout</span>',
+})
+export class MockLogoutComponent { }
