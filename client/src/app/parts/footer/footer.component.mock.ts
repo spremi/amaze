9b2991faa2586f9013cfd90361a3b2338481a22d
Sanjeev Premi <spremi@ymail.com>
2019-09-18 00:18:41 +0530

    test: c: parts: footer: Add mock
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/footer/footer.component.mock.ts
@@ -0,0 +1,15 @@
+//
+// [amaze-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-footer',
+  template: '<span>Mock Footer</span>',
+})
+export class MockFooterComponent { }
