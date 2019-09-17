c078d98eab92c5896397e9c099fbe29463622d70
Sanjeev Premi <spremi@ymail.com>
2019-09-17 23:04:04 +0530

    chore: c: parts: Add 'header'
    Command:
      ng g c parts/header --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './parts/header/header.component';
 import { AboutComponent } from './routes/about/about.component';
 import { BookletComponent } from './routes/booklet/booklet.component';
 import { GalleryComponent } from './routes/gallery/gallery.component';
@@ -22,6 +23,7 @@ import { ShowComponent } from './routes/show/show.component';
     BookletComponent,
     PageComponent,
     ShowComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
